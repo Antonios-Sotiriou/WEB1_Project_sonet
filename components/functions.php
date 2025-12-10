5aa35318bcf31a4cc5327f15441c6e800b66fa47
Antonios Sotiriou <antonios.sotiriou@hotmail.gr>
2025-12-10 19:14:38 +0100

    Interacting with the database through prepare statements API, to avoid sql injections. Changed half of the commands. The rest will be changed also.

--- components/functions.php
+++ components/functions.php
@@ -17,21 +17,29 @@ function dbconnect() {
 function userLogIn($conn, $info) {
     $email = htmlspecialchars($info["email"]);
 
-    $retrieve_user = "SELECT * FROM users WHERE email = '$email'";
-    $result = $conn->query($retrieve_user);
-    if ($result->num_rows > 0) {
-        $row = $result->fetch_assoc();
+    $retrieve_user = $conn->prepare("SELECT * FROM users WHERE email = ?");
+    $retrieve_user->bind_param("s", $email);
+    $retrieve_user->execute();
+    if ($retrieve_user == true) {
 
-        if (password_verify($info["password"], $row["password"])) {
-            session_start();
-            $_SESSION["email"] = $row["email"];
-            header("Location: home.php");
+        $result = $retrieve_user->get_result();
+
+        if ($result->num_rows > 0) {
+            $row = $result->fetch_assoc();
+
+            if (password_verify($info["password"], $row["password"])) {
+                session_start();
+                $_SESSION["email"] = $row["email"];
+                header("Location: home.php");
+            } else {
+                raise_error("Invalid password.");
+            }
         } else {
-            raise_error("Invalid password.");
+            raise_error("User not found! Check your email address for type mistakes!");
         }
-    } else {
-        raise_error("User not found! Check your email address for type mistakes!");
     }
+    $retrieve_user->close();
+    $result->close();
 }
 
 function verifyFirstName($firstName) {
@@ -80,21 +88,38 @@ function userRegister($conn, $_post) {
 
     if ($_post["password"] === $_post["repeat_password"]) {
 
+        if (strlen($_post["password"]) < 6) {
+            raise_error("Password must be at least 6 characters long.");
+            return;
+        }
+
         $email = filter_var($_post["email"], FILTER_VALIDATE_EMAIL);
         $hash = password_hash($_post["password"], PASSWORD_DEFAULT);
 
-        $check_email = "SELECT * FROM users WHERE email='$email'";
-        $result = $conn->query($check_email);
-        if ($result->num_rows > 0) {
-            echo "Email address already exists!";
-        } else {
-            $insertQuery = "INSERT INTO users(first_name, last_name, email, password) VALUES ('$first_name', '$last_name', '$email', '$hash')";
-            if ($conn->query($insertQuery) == TRUE) {
-                header("location: login.php");
+        $check_email = $conn->prepare("SELECT * FROM users WHERE email = ?");
+        $check_email->bind_param("s", $email);
+        $check_email->execute();
+
+        if ($check_email) {
+
+            $result = $check_email->get_result();
+
+            if ($result->num_rows > 0) {
+                raise_error("Email address already exists!");
             } else {
-                raise_error("An error has occured!".htmlspecialchars($conn->error));
+                $insertQuery = $conn->prepare("INSERT INTO users(first_name, last_name, email, password) VALUES (?, ?, ?, ?)");
+                $insertQuery->bind_param("ssss", $first_name, $last_name, $email, $hash);
+                $insertQuery->execute();
+                if ($insertQuery == TRUE) {
+                    $insertQuery->close();
+                    header("location: login.php");
+                } else {
+                    raise_error("An error has occured!".htmlspecialchars($conn->error));
+                }
             }
         }
+        $check_email->close();
+        $result->close();
     } else {
         raise_error('Passwords not matching. Please check for typing mistakes.');
     }
@@ -110,7 +135,7 @@ function userUpdate($conn, $_post, $user): int {
         if (empty($first_name)) {
             $error = 1;
         } else {
-            $queries_array[] = "UPDATE users SET first_name = '$first_name' WHERE user_id = $user_id";
+            $queries_array[] = $conn->prepare("UPDATE users SET first_name = '$first_name' WHERE user_id = $user_id");
         }
     }
 
@@ -119,13 +144,14 @@ function userUpdate($conn, $_post, $user): int {
         if (empty($last_name)) {
             $error = 1;
         } else {
-            $queries_array[] = "UPDATE users SET last_name = '$last_name' WHERE user_id = '$user_id'";
+            $queries_array[] = $conn->prepare("UPDATE users SET last_name = '$last_name' WHERE user_id = '$user_id'");
         }
     }
 
     if (!empty($_FILES["uploadPhoto"]["name"])) {
 
         if (!uploadImage($conn, $_FILES["uploadPhoto"], $user)) {
+            raise_error("File update failed!");
             $error = 1;
         }
     }
@@ -133,7 +159,8 @@ function userUpdate($conn, $_post, $user): int {
         return 0;
     } else {
         foreach ($queries_array as $query) {
-            $conn->query($query);
+            $query->execute();
+            $query->close();
         }
     }
     header('Location: profile.php?firstName='.$user["first_name"].'&lastName='.$last_name.'&user_id='.$user_id);
@@ -162,24 +189,41 @@ function uploadImage($conn, $img, $user): int {
     if (move_uploaded_file($_FILES["uploadPhoto"]["tmp_name"], $destination)) {
         $img_name = $_FILES["uploadPhoto"]["name"];
 
-        $check_entry = "SELECT * FROM prof_images WHERE user_id = '$user[user_id]'";
-        $result = $conn->query($check_entry);
-        if ($result->num_rows > 0) {
-            $updateQuery = "UPDATE prof_images SET img_name = '$img_name' WHERE user_id = '$user[user_id]'";
-            if ($conn->query($updateQuery) == TRUE) {
+        $check_entry = $conn->prepare("SELECT * FROM prof_images WHERE user_id = ?");
+        $check_entry->bind_param("i", $user["user_id"]);
+        $check_entry->execute();
+
+        if ($check_entry == true) {
+            $result = $check_entry->get_result();
+            $check_entry->close();
+
+            if ($result->num_rows > 0) {
+                $result->close();
+
+                $updateQuery = $conn->prepare("UPDATE prof_images SET img_name = ? WHERE user_id = ?");
+                $updateQuery->bind_param("si", $img_name, $user["user_id"]);
+                $updateQuery->execute();
+                if ($updateQuery == false) {
+                    return 0;
+                }
+                $updateQuery->close();
                 header('Location: profile.php?firstName='.$user["first_name"].'&lastName='.$user["last_name"].'&user_id='.$user["user_id"]);
             } else {
-                raise_error("File update failed!");
-                return 0;
-            }
-        } else {
-            $insertQuery = "INSERT INTO prof_images (user_id, img_name) VALUES ('$user[user_id]', '$img_name')";
-            if ($conn->query($insertQuery) == TRUE) {
+                $result->close();
+
+                $insertQuery = $conn->prepare("INSERT INTO prof_images (user_id, img_name) VALUES (?, ?)");
+                $insertQuery->bind_param("is", $user["user_id"], $img_name);
+                $insertQuery->execute();
+                if ($insertQuery == false) {
+                    return 0;
+                }
+                $insertQuery->close();
                 header('Location: profile.php?firstName='.$user["first_name"].'&lastName='.$user["last_name"].'&user_id='.$user["user_id"]);
             }
+        } else {
+            return 0;
         }
     } else {
-        raise_error("File upload failed!");
         return 0;
     }
 
@@ -208,24 +252,31 @@ function fetchCurrentUser($conn) {
 
     if (isset($_SESSION["email"])) {
         $email = $_SESSION["email"];
-        $query = mysqli_query($conn, 
+        $query = $conn->prepare( 
             "SELECT users.user_id, users.first_name, users.last_name, users.email, prof_images.img_name 
             FROM users 
             LEFT JOIN prof_images ON users.user_id = prof_images.user_id 
-            WHERE users.email='$email'"
+            WHERE users.email=?"
         );
+        $query->execute([$email]);
+        printObjectMethods($query);
+        if ($query == true) {
 
-        while ($row = mysqli_fetch_array($query)) {
-            $globals["user_id"] = $row["user_id"];
-            $globals["first_name"] = $row["first_name"];
-            $globals["last_name"] = $row["last_name"];
-            $globals["email"] = $row["email"];
-            $globals["md_email"] = md5($row["email"]);
-            if (!empty($row["img_name"])) {
-                $globals["profile_image"] = "media/".$globals["md_email"]."/".$row["img_name"];
-            } else {
-                $globals["profile_image"] = "images/default_user.jpg";
+            $result = $query->get_result();
+
+            while ($row = $result->fetch_assoc()) {
+                $globals["user_id"] = $row["user_id"];
+                $globals["first_name"] = $row["first_name"];
+                $globals["last_name"] = $row["last_name"];
+                $globals["email"] = $row["email"];
+                $globals["md_email"] = md5($row["email"]);
+                if (!empty($row["img_name"])) {
+                    $globals["profile_image"] = "media/".$globals["md_email"]."/".$row["img_name"];
+                } else {
+                    $globals["profile_image"] = "images/default_user.jpg";
+                }
             }
+            $result->close();
         }
     } else {
         $globals["user_id"] = 0;
@@ -234,6 +285,7 @@ function fetchCurrentUser($conn) {
         $globals["email"] = "";
         $globals["profile_image"] = "images/default_user.jpg";
     }
+    $query->close();
 
     return $globals;
 }
@@ -482,5 +534,19 @@ function raise_error($error_message) {
 function enableDebugging() {
     error_reporting(E_ALL);
     ini_set('display_errors', 'On');
+}
+function printObjectMethods($obj) {
+    $methods_array = get_class_methods($obj);
+    print_r($obj);
+    echo "<br><br>";
+    echo $obj->field_count;
+    echo "<br><br>";
+
+
+    echo "Object Methods {<br>";
+    foreach ($methods_array as $method) {
+        echo "&nbsp&nbsp&nbsp&nbsp".$method."<br>";
+    }
+    echo "}<br><br>";
 }
 ?>
